6c619b2d909cc21e2ab18ca411f85bcdf96b6912
kushagrbhatia <bhatia.kushagr80@gmail.com>
2024-07-30 22:57:25 -0500

    Create rightsizing.py

---
+++ rightsizing.py
@@ -0,0 +1,4 @@
+sd_CpuCont95_list = [x for x in sd_CpuCont95_list if not np.isnan(x)]
+sd_CpuDemand95_list = [x for x in sd_CpuDemand95_list if not np.isnan(x)]
+sd_MemUsage95_list = [x for x in sd_MemUsage95_list if not np.isnan(x)]
+sd_CpuUsage95_list = [x for x in sd_CpuUsage95_list if not np.isnan(x)]
